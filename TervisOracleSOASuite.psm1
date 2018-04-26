1c259a4698769d0739c3486ef585c2dd68a70a32
ChrisMagnuson <hourglasssand@gmail.com>
2018-04-26 15:35:58 -0400

    Refactor into functions

--- TervisOracleSOASuite.psm1
+++ TervisOracleSOASuite.psm1
@@ -1,24 +1,56 @@
-﻿function Invoke-TervosOracleSOAJobMonitoring {
-    $xml = Invoke-RestMethod http://soaweblogic.production.tervis.prv:7201/SOAScheduler/soaschedulerservlet?action=read
-    $CleanedUpXML = $xml -replace '&nbsp;',''
-    $Table = [xml]$CleanedUpXML
+﻿Invoke-PSDepend -Import -Force
 
-    function Invoke-SchedulerJobParseDate {
-        begin {
-            $DateFormat = "ddd MMM dd HH:mm:ss EDT yyyy"
-        }
-        process {
-            if ($_ -ne "null") {
-                [DateTime]::ParseExact($_, $DateFormat, $Null)
-            } else {
-                $_
-            }
+function Get-SOASchedulerURL {
+    if ($env:SOASchedulerURL) {
+        $env:SOASchedulerURL
+    } else {
+        $Script:SOASchedulerURL
+    }
+}
+
+function Set-SOASchedulerURL {
+    param (
+        $SOASchedulerURL
+    )
+    $Script:SOASchedulerURL = $SOASchedulerURL
+}
+
+function Get-NotificationEmailAddress {
+    if ($env:NotificationEmailAddress) {
+        $env:NotificationEmailAddress
+    } else {
+        $Script:NotificationEmailAddress
+    }
+}
+
+function Set-NotificationEmailAddress {
+    param (
+        $NotificationEmailAddress
+    )
+    $Script:NotificationEmailAddress = $NotificationEmailAddress
+}
+
+function Invoke-SchedulerJobParseDate {
+    begin {
+        $DateFormat = "ddd MMM dd HH:mm:ss EDT yyyy"
+    }
+    process {
+        if ($_ -ne "null") {
+            [DateTime]::ParseExact($_, $DateFormat, $Null)
+        } else {
+            $_
         }
     }
+}
 
-    $SchedulerJobs = $Table.table.tr |
+function Get-SOASchedulerJob {
+    $xml = Invoke-RestMethod -Uri (Get-SOASchedulerURL)
+    $CleanedUpXML = $xml -replace '&nbsp;',''
+    $Table = [xml]$CleanedUpXML
+ 
+    $Table.table.tr |
     Select-Object -Skip 1 |
-    % {
+    ForEach-Object {
         [PSCustomObject][Ordered]@{
             Name = $_.td[0].'#text'
             NextRun = $_.td[2].'#text' | Invoke-SchedulerJobParseDate
@@ -30,5 +62,24 @@
     Add-Member -MemberType ScriptProperty -Name TimeAfterWhichToTriggerAlert -PassThru -Value {
         $TimeSpamBetweenRuns = $This.NextRun - $This.PreviousRun
         $This.NextRun + $TimeSpamBetweenRuns
+    }
+}
+
+function Invoke-TervosOracleSOAJobMonitoring {
+    $SchedulerJobs = Get-SOASchedulerJob 
+
+    $JobsThatDontNeedToRun = "WarrantyOrderJob", "WebWarrantyJob", "WOMZRJob", "ImageIntJob"
+
+    $JobsNotWorking = $SchedulerJobs | 
+    Where-Object Name -NotIn $JobsThatDontNeedToRun |
+    Where-Object TimeAfterWhichToTriggerAlert -lt (Get-Date)
+
+    if ($JobsNotWorking) {
+        $OFSBackup = $OFS
+        $OFS = ""
+        Send-TervisMailMessage -To $env:NotificationEmailAddress -From $env:NotificationEmailAddress -Subject "SOA Jobs failing" -BodyAsHTML -Body @"
+$($JobsNotWorking | ConvertTo-Html)
+"@
+        $OFS = $OFSBackup
     }
 }
